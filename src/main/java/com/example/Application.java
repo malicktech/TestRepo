ff2752f5db0273e895f87072093e244acaacdb70
Malick <diop-malick@hotmail.fr>
2016-03-29 01:29:27 +0200

    simplify config

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,16 +1,22 @@
 package com.example;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-import com.example.config.AppConfig;
+import com.example.config.BeanConfig;
+import com.example.config.JsfConfig;
+import com.example.config.WebAppConfig;
 
 // @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.example" })
+@Import({ JsfConfig.class, BeanConfig.class, WebAppConfig.class
+	// FacesRewriteConfigurationProvider.class,
+	})
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/config/WebAppConfig.java
@@ -0,0 +1,17 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebAppConfig {
+
+	// @Bean
+	// public UrlBasedViewResolver faceletsViewResolver() {
+	// UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+	// resolver.setViewClass(JsfView.class);
+	// resolver.setPrefix("/WEB-INF/");
+	// resolver.setSuffix(".xhtml");
+	// return resolver;
+	// }
+
+}
